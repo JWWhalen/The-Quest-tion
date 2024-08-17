50fa940130a5b64038a3229e093ecb81dd16a468
Justin Whalen <138682790+JWWhalen@users.noreply.github.com>
2024-08-16 23:25:01 -0230

    Updates

---
+++ electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional, include if you use preload
+    },
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html')); // Load the built index.html file
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
